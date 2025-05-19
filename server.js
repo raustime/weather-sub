89f5ea46af5c93c503fe58d15d52ec331d8a0f13
Устименко Роман Олексійович <ustimenko_ro@corp.imp.lg.ua>
2025-05-19 15:02:42 +0300

    added address 0.0.0.0

--- server.js
+++ server.js
@@ -63,7 +63,8 @@ app.use((req, res) => {
 });
 
 // Старт сервера
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
